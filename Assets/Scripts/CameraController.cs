5ffe6b8975d695e39f06290a9afcd73736c9155d
Paulo Pereira <rising.gamedev@gmail.com>
2020-05-02 14:37:24 +0100

    # Added CameraController script # Added PlayerController script

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+	public float sensitivity = 100f;
+
+	Transform playerBody;
+
+	float mouseX = 0f;
+	float mouseY = 0f;
+	float cameraXRotation = 0f;
+
+	void Start() {
+		Cursor.lockState = CursorLockMode.Locked;
+		playerBody = transform.parent;
+	}
+
+	void Update()
+    {
+		mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime; 
+		mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+
+		cameraXRotation -= mouseY;
+		cameraXRotation = Mathf.Clamp(cameraXRotation, -90f, 90f);
+
+		transform.localRotation = Quaternion.Euler(cameraXRotation, 0f, 0f);
+		playerBody.Rotate(Vector3.up, mouseX);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+
+	[Space]
+	[Header("Movement Properties")]
+	public float movementSpeed = 12f;
+	public float jumpHeight = 12f;
+	public float gravity = -9.8f;
+
+	[Space]
+	[Header("Ground Check Properties")]
+	public Transform groundCheck;
+	public float groundCheckRadius = .4f;
+	public LayerMask groundMask;
+
+	CharacterController characterController;
+	Vector3 movement;
+	float xMove;
+	float zMove;
+
+	Vector3 velocity;
+
+	bool isGrounded = true;
+
+	void Start() {
+		characterController = GetComponent<CharacterController>();
+	}
+
+	void Update()
+    {
+		isGrounded = IsGrounded();
+		Move(Time.deltaTime);
+		Jump();
+		ApplyGravity(Time.deltaTime);
+	}
+
+	private bool IsGrounded() {
+		if (groundCheck == null) return true;
+
+		return Physics.CheckSphere(groundCheck.position, groundCheckRadius, groundMask);
+	}
+
+	private void Move(float deltaTime) {
+		xMove = Input.GetAxis("Horizontal");
+		zMove = Input.GetAxis("Vertical");
+
+		movement = transform.right * xMove + transform.forward * zMove;
+
+		characterController.Move(movement * movementSpeed * deltaTime);
+	}
+
+	private void Jump() {
+		if (Input.GetKeyDown(KeyCode.Space) && isGrounded) {
+			velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+		}
+	}
+
+	private void ApplyGravity(float deltaTime) {
+		if (isGrounded && velocity.y < 0)
+			velocity.y = -2f;
+
+		velocity.y += .5f * gravity * deltaTime;
+
+		characterController.Move(velocity * deltaTime);
+	}
+}
